7f3cc62fc658ed619a4dda6a0930fa82f252c05c
SwapnilDwivedi77 <62413591+SwapnilDwivedi77@users.noreply.github.com>
2020-10-20 10:31:18 +0530

    Recursion | Tower of Hanoi

---
+++ GeeksForGeeksCourse/3-Recursion/ToweOfHanoi.cpp
@@ -0,0 +1,47 @@
+/* The tower of Hanoi is a famous puzzle where we have three rods and N disks.
+ * The objective of the puzzle is to move the entire stack to another rod.
+ * You are given the number of discs N. Initially, these discs are in the rod 1.
+ * You need to print all the steps of discs movement so that all the discs reach the 3rd rod.
+ * Also, you need to find the total moves.
+ * Note: The discs are arranged such that the top disc is numbered 1 and the bottom-most disc is numbered N.
+  Also, all the discs have different sizes and a bigger disc cannot be put on the top of a smaller disc. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function prints the steps and returns total number of steps
+    long long toh(int N, int from, int to, int aux)
+    {
+
+        if (N == 1)
+        {
+            cout << "move disk " << N << " from rod " << from << " to " << to << endl;
+            return 1;
+        }
+        toh(N - 1, from, aux, to);
+
+        cout << "move disk " << N << " from rod " << from << " to " << to << endl;
+
+        toh(N - 1, aux, to, from);
+
+        return pow(2, N) - 1;
+    }
+};
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        long long moves = ob.toh(N, 1, 3, 2);
+        cout << moves << endl;
+    }
+    return 0;
+}
